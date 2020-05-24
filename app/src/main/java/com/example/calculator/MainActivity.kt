b08bf6ea81dd7e2476161bae3f23e516af1895c1
fehad <fehadaminkunroo@gmail.com>
2020-05-25 05:00:14 +0530

    3rd commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -84,4 +84,36 @@ class MainActivity : AppCompatActivity() {
         buttonPlus.setOnClickListener(opListener)
 
     }
+
+    private fun performOperation(value:String,operation:String)
+    {
+        if(operand1 == null)
+        {
+            operand1 = value.toDouble()
+        }
+        else
+        {
+            operand2 = value.toDouble()
+
+            if(pendingOperation == "=")
+            {
+                pendingOperation = operation
+            }
+            when (pendingOperation){
+                "=" -> operand1 = operand2
+                "/" -> if(operand2 == 0.0)
+                            {
+                                operand1 = Double.NaN
+                            }
+                        else{
+                                operand1 =operand1!!/ operand2
+                            }
+                "*" -> operand1 = operand1!! * operand2
+                "-" -> operand1 = operand1!! - operand2
+                "+" -> operand1 = operand1!! + operand2
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
+    }
 }
